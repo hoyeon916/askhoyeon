4259f46836260927a585ac7cb711008e64846ade
hoyon <hoyon916oo@naver.com>
2017-04-01 13:38:40 +0900

    askhoyeon

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+	# author = models.ForeignKey('auth.User')  둘다 같음. AUTH_USER_MODEL 에 auth.User 있음
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default = timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
